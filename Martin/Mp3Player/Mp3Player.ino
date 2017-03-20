258a631222ec76deddc01344734c06880d5fb4ee
Delta135 <135.delta@gmail.com>
2017-03-20 09:34:25 +0100

    Added net code

--- Martin/Mp3Player/Mp3Player.ino
+++ Martin/Mp3Player/Mp3Player.ino
@@ -3,7 +3,12 @@
 #include "Song.h"
 #include <DFRobotDFPlayerMini.h>
 #include <LiquidCrystal.h>
-//#include <TimedAction.h>
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
 
 //#################-Lightshow settings
 const byte rgbLed[] = {2,3,4, 5,6,7}; //order is R,G,B,R,G,B 
@@ -56,6 +61,32 @@ byte timeCodeMin, timeCodeSec; //used for time code
 byte currentSong;
 const int LCDUpdateIntaval = 1000; // 1 sec
 unsigned long LCDPreviousMillis = 0;
+
+//#################-Net
+// if we use dhcp to get IP, then we can use this connection to display the result of the dhcp request.
+int connection;
+
+// Defining the mac address to use for the board.
+byte mac[] = { 0xAF, 0xEB, 0xCA, 0xDB, 0xFA, 0xDE };
+
+// The client wich will connect to the server.
+EthernetClient client;
+
+// When we is going to send to our Azure server we will use this server variable instead of the IP.
+// char server[] = "http://citybuilder.azurewebsites.net/sample/hello";
+
+// When not using Azure server, we set the ip of the server static, wich we will connect to.
+IPAddress server(192, 168, 1, 108);
+
+// Using this if we dont want to use dhcp to get ip for the Arduion board.
+IPAddress ip(192, 168, 1, 135);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+
+// The string of methods that this arduino is holding, and should sent to the server.
+String methodStr = "RockOn#test123,0,0,1,0,,#test321,0,0,1,0,Vol,5";
+
 //------------------------------------------------Arduino stuff
 
 void setup()
@@ -79,6 +110,14 @@ void loop()
 	{
 		printDetail(mp3Player.readType(), mp3Player.read()); //Print the detail message from DFPlayer to handle different errors and states.
 	}
+
+	//renews the IP lease - returning a byte with the following:
+	//0: nothing happened
+	//1: renew failed
+	//2: renew success
+	//3: rebind fail
+	//4: rebind success
+	byte result = Ethernet.maintain();
 
 	//if (mp3IsPlayering && (unsigned long)(millis() - LCDPreviousMillis) > LCDUpdateIntaval)
 	//{
@@ -643,3 +682,62 @@ void loopMp3(bool loop)
 
 //------------------------------------------------Net Code
 
+void initNet()
+{
+	// Checking that we are inside setup.
+	Serial.println("We are inside setup method for NETCODE!");
+
+	// Just starting the ethernet port and setting mac, ip, gateway, subnetmask.
+	Ethernet.begin(mac, ip, gateway, subnet);
+
+	// If we where using dhcp we could print out the status of the lease.
+	// This should be used where we begin the Ethernet.begin like this connection = Ethernet.begin();
+	// returns state of connection 1 for succes 0 for failure
+	// Serial.println(connection);
+
+	// Letting the Ethernet port finishing initializing before we try to do the connection. 
+	delay(1000);
+
+	Serial.println("Trying to connect...");
+	// Connecting to the server.
+	int connectionStatus = connectToServer();
+
+	if (connectionStatus == 1) {
+
+		Serial.println("connected");
+
+	}
+	else if (connectionStatus == 0) {
+		Serial.println("connection failed");
+	}
+
+	// Checking if the client connection is still open to the server, else we close it.
+	if (client.available())
+	{
+		Serial.println("There is still connection to the server");
+	}
+	else {
+		client.stop();
+		Serial.println("Connection Terminated");
+	}
+}
+
+// The connection method when we want to connect to the server, is called inside setup().
+int connectToServer() {
+
+	if (client.connect(server, 1000)) {
+
+		// Trying to sent method string to the server.
+		byte bytesSent = client.print(methodStr);
+
+		// Returningn status on the connection.
+		return 1;
+
+	}
+	else {
+
+		// Returningn status on the connection.
+		return 0;
+
+	}
+}
